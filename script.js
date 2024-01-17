e6a0211b87ce38ae03519fa04c70879c631065e1
Kanith Matayangkul <kanith.coder@gmail.com>
2024-01-17 22:51:32 +0700

    Step 38

--- script.js
+++ script.js
@@ -21,3 +21,7 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
     console.log("Going to store.");
 }
+
+function goCave() {
+    console.log("Going to cave.");
+}
