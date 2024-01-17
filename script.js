f2dd4f6ae00c0450905cdf6f918b071104a4cc3b
Kanith Matayangkul <kanith.coder@gmail.com>
2024-01-17 23:16:19 +0700

    Step 47

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function goStore() {
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
 
-    // button1.onclick = buyHealth;
-    // button2.onclick = buyWeapon;
-    // button3.onclick = goTown;
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
 
     text.innerText = "You enter the store.";
 }
@@ -36,6 +36,15 @@ function goCave() {
 
 function fightDragon() {
     console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+}
+
+function buyWeapon() {
+}
+
+function goTown() {
 }
 
 // hello world
