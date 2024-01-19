aa68639f39fb39af5c80eb77c1996750b2c89aad
kaemanee <kaemaneej@gmail.com>
2024-01-19 17:29:13 +0700

    Step 44

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore()
 {
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 }
 function goCave()
 {
