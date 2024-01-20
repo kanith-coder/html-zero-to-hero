452af6c8c7ed5c01dc78fcc866972091e1d6668e
phorn <ratchadapornhomjan@gmail.com>
2024-01-20 22:18:29 +0700

    step 44

--- script.js
+++ script.js
@@ -23,21 +23,26 @@ const monsterStats  = document.querySelector("#monsterStats")
 const monsterName  = document.querySelector("#monsterName")
 const monsterHealthText  = document.querySelector("##monsterHealth")
 
-function goStore () {
-        // console.log("Going to store.");
-        button1.innerText = "Buy 10 health (10 gold)";
-}
+// function goStore () {
+//         // console.log("Going to store.");
+//         button1.innerText = "Buy 10 health (10 gold)";
+// }
 
-function goCave  () {
-        console.log("Going to cave.");
-}
+// function goCave  () {
+//         console.log("Going to cave.");
+// }
 
-function fightDragon   () {
-        console.log("Fighting dragon.");
-}
+// function fightDragon   () {
+//         console.log("Fighting dragon.");
+// }
 
 //initialize buttons
 
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goStore() {
+        button1.innerText = "Buy 10 health (10 gold)";
+        button2.innerText = "Buy weapon (30 gold)";
+        button3.innerText = "Go to town square";
