132679aac3b424045dc14ab4484ad524024001be
phorn <ratchadapornhomjan@gmail.com>
2024-01-21 17:53:02 +0700

    step 47

--- script.js
+++ script.js
@@ -55,11 +55,21 @@ button3.onclick = fightDragon;
         //         }
 
         function goStore() {
-                text.innerText = "You enter the store.";
+                button1.innerText = "Buy 10 health (10 gold)";
                 button2.innerText = "Buy weapon (30 gold)";
                 button3.innerText = "Go to town square";
                 button1.onclick = buyHealth;
                 button2.onclick = buyWeapon;
                 button3.onclick = goTown;
-                }
+                text.innerText = "You enter the store.";
 
+        function buyHealth () {
+
+                }
+        function buyWeapon () {
+
+                }
+        function goTown () {
+
+                }
+                
