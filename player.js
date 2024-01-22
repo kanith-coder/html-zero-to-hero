57dfbd1ebd5129a66e24af8fe9228bf82e8f2421
Kanith Matayangkul <kanith.coder@gmail.com>
2024-01-22 08:39:04 +0700

    player script

--- player.js
+++ player.js
@@ -5,15 +5,46 @@ const data = [
     { name: "Alex", xp: 44, health: 45, gold: 80 }
 ];
 
+// Array
+let array = [2, 'hello', true, {age: 25}, [1, 2, 3, 4, 5]];
+
+// Object
+const kanith = {
+    weigth: 60,
+    height: 170,
+    name: 'kanith',
+    address: {
+        city: 'bangkok',
+        country: 'thailand'
+    }
+};
+
 function addPlayer(newPlayer) {
     const tbody = document.querySelector("#player tbody");
     newPlayer.forEach(function(item) {
-        var row = document.createElement('tr');
-        Object.values(item).forEach(function(value) {
+        // <tr></tr>
+        var row = document.createElement('tr'); 
+        // { name: "John", xp: 47, health: 46, gold: 100 }
+        Object.values(item).forEach(function(key, value) {
+            // <td>John</td>
             var td = document.createElement('td');
             td.textContent = value;
+            // <tr>
+            // <td>John</td>
+            // <td>47</td>
+            // <td>46</td>
+            // <td>100</td>
+            // </tr>
             row.appendChild(td);
         });
+        // <tbody>
+        // <tr>
+            // <td>John</td>
+            // <td>47</td>
+            // <td>46</td>
+            // <td>100</td>
+            // </tr>
+        // </tbody>
         tbody.appendChild(row);
     });
 }
