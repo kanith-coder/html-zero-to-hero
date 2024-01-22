3b2a8c18f04872cf8f857274818e18e63bc3fcee
Kanith Matayangkul <kanith.coder@gmail.com>
2024-01-22 16:50:04 +0700

    109

--- script.js
+++ script.js
@@ -35,6 +35,19 @@ const weapons = [
     power: 100,
   },
 ];
+const monsters = [{
+    name: "slime",
+    level: 2,
+    health: 15
+}, {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+}, {
+    name: "dragon",
+    level: 20,
+    health: 300
+}];
 const locations = [
   {
     name: "town square",
@@ -94,11 +107,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-  alert("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     health += 10;
@@ -149,6 +157,11 @@ function sellWeapon() {
 function fightSlime() {}
 
 function fightBeast() {}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+    alert("Fighting dragon.");
+  }
 
 // hello world
 
