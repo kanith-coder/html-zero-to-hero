904c7fcc99c1146171841ba67e7cad69dece6e98
Kanith Matayangkul <kanith.coder@gmail.com>
2024-01-22 20:53:26 +0700

    119

--- script.js
+++ script.js
@@ -182,7 +182,10 @@ function goFight() {
     monsterHealthText.innerText = monsterHealth;
 }
 
-function attack() {}
+function attack() {
+    text.innerText = "The "+ monsters[fighting].name +" attacks.";
+}
+
 function dodge() {}
 
 // hello world
