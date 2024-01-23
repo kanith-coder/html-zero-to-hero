cabb82606fe73fb5c22ca715458b962d614fecdc
phorn <ratchadapornhomjan@gmail.com>
2024-01-23 21:04:47 +0700

    step 76

--- script.js
+++ script.js
@@ -74,7 +74,9 @@ function fightDragon() {
       
 function buyHealth() {
   gold -= 10;
-  health += 10;  
+  health += 10;
+  goldText.innerText = gold;
+  healthText.innerText = health;  
   }
       
 function buyWeapon() {
