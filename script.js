26c3a7233d1a48b30d064af81dcd340b3d1207e3
phorn <ratchadapornhomjan@gmail.com>
2024-01-30 17:26:47 +0700

    Step 4

--- script.js
+++ script.js
@@ -9,49 +9,40 @@ let incentive= 0;
 
 depositBtn.addEventListener('click', (event) => {
     balance += 10;
-    deposit();
   });
   
-depositTxt.addEventListener('keyup', (event) => {
-    event.preventDefault; 
-    incentive += 50;
-    deposit();
+depositTxt.addEventListener('keyup', function (event){
+    this.valueAsnumber;
+    console.log(this.valueAsnumber);
     if (event.key === 'Enter'){
-      console.log('enter');
+      event.preventDefault;
+      addRow();
     }
   });
   
-  function deposit() {
-    console.log('Deposit button clicked');
-    depositTxt.valueAsNumber;
-    depositTxt.value;
-  
-    balanceSpn.innerText = balance;
-    incentiveSpn.innerText = incentive;
-  
-    const tbody = document.querySelector('#deposit-money tbody');
-    var row = document.createElement('tr');
-  
-    // Format the date using Intl.DateTimeFormat
-    var formattedDate = new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    }).format(Date.now());
-    // Date
-    var tdDate = document.createElement('td');
-    tdDate.textContent = formattedDate;
-    row.appendChild(tdDate);
-  
-    // Amount
-     // Format the number as currency using Intl.NumberFormat
-     var formattedCurrency = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD' // Change this to your desired currency code
-    }).format(500);
-    var tdAmount = document.createElement('td');
-    tdAmount.textContent = formattedCurrency;
-    row.appendChild(tdAmount);
-    
-    tbody.appendChild(row);
-  }
+function addRow() {
+  //  <tbody></tbody>
+  const tbody = document.querySelector('#deposit-money tbody');
+  //  <tr></tr>
+  const row = document.createElement('tr');
+  //  <td></td>
+  const tdDate = document.createElement('td');
+  //  <td>01/01/2567</td>
+  const currentDate = new Date().toLocaleDateString();
+  tdDate.textContent = currentDate;
+  //  <tr>
+  //  <td>01/01/2567</td>
+  //  </tr>
+  row.appendChild(tdDate);
+
+  const tdMoney = document.createElement('td');
+  //  <td>2000</td>
+  tdMoney.textContent = 2000;
+  //  <tr>
+  //  <td>01/01/2567</td>
+  //  <td>2000</td>
+  //  </tr>
+  row.appendChild(tdMoney);
+  tbody.appendChild(row);
+}
+
