558d4038fde61133f583d7ef4376fe4814dc0d8d
phorn <ratchadapornhomjan@gmail.com>
2024-02-11 21:39:16 +0700

    step1 mockup

--- script.js
+++ script.js
@@ -1,29 +1,73 @@
- //ไปยัง form หน้าที่ 2
-function nextStep() {
+//การทำงานปุ่ม
+function nextPage2() {
   document.getElementById('bookingForm').style.display = 'none';
   document.getElementById('personalInfoForm').style.display = 'block';
 }
 
-// ย้อนกลับไปยัง form หน้าที่ 1
-function prevStep() {
-  document.getElementById('bookingForm').style.display = 'block';
-  document.getElementById('personalInfoForm').style.display = 'none';
+function prevPage1() {
+  document.getElementById('bookingForm').style.display = 'block';       //page1
+  document.getElementById('personalInfoForm').style.display = 'none';   //page2
 }
 
-// ไปยัง form หน้าที่ 3
-function nextStep2() {
+function nextPage3() {
   document.getElementById('personalInfoForm').style.display = 'none';
   document.getElementById('additionalInfoForm').style.display = 'block';
 }
 
-// ย้อนกลับไปยัง form หน้าที่ 2
-function prevStep2() {
-  document.getElementById('personalInfoForm').style.display = 'block';
-  document.getElementById('additionalInfoForm').style.display = 'none';
+function prevPage2() {
+  document.getElementById('personalInfoForm').style.display = 'block';       //page1
+  document.getElementById('additionalInfoForm').style.display = 'none';   //page2
 }
 
-// function submitForm() {
-//   // จัดการการส่งข้อมูลหลังจากกรอก form หน้าที่ 3
-//   alert('Booking successful!'); // สามารถแทนที่ด้วยโค้ดที่จัดการการส่งข้อมูล
-// }
-1
+
+// **ขาไป** ให้checkbox ได้ 1รายการ
+function limitCheckDeparture(checkbox) {
+  var checkboxes = document.getElementsByName("departureCheck");
+
+  for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] !== checkbox) {
+          checkboxes[i].checked = false;
+      }
+  }
+
+  updateTotalPrice();
+}
+
+// **ขากลับ** ให้checkbox ได้ 1รายการ
+function limitCheckReturn(checkbox) {
+  var checkboxes = document.getElementsByName("returnCheck");
+
+  for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] !== checkbox) {
+          checkboxes[i].checked = false;
+      }
+  }
+
+  updateTotalPrice();
+}
+
+// ราคาตั๋ว
+function updateTotalPrice() {
+  var departurePrice = getCheckedOptionValue("departureCheck");
+  var returnPrice = getCheckedOptionValue("returnCheck");
+  var passengerCount = parseInt(document.getElementById("passengerCount").value);
+
+  var totalPrice = (departurePrice + returnPrice) * passengerCount;
+
+  document.getElementById("totalPrice").innerHTML = "<strong>" + totalPrice.toLocaleString('en-US', {style: 'currency', currency: 'THB'}) + "</strong>";
+}
+
+function getCheckedOptionValue(name) {
+  var checkboxes = document.getElementsByName(name);
+
+  for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+          return parseInt(checkboxes[i].value);
+      }
+  }
+
+  return 0;
+}
+
+
+
