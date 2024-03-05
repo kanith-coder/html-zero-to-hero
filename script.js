18a12fbc4b06b232be6ce88562aa5dd71e490a6f
Kanith <kanith.coder@gmail.com>
2024-03-05 09:20:23 +0700

    Get Api

---
+++ script.js
@@ -0,0 +1,14 @@
+function getApi() {
+axios.get('https://dummy.restapiexample.com/api/v1/employees')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+}
