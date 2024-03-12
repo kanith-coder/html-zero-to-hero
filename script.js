05dc81245d5295751936afbee6d97730e0801457
Ratchadapond <ratchadapornhomjan@gmail.com>
2024-03-12 21:36:37 +0700

    show product

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function getApi() {
-axios.get('https://pubpup-tour-default-rtdb.asia-southeast1.firebasedatabase.app/products.json')
-  .then(function (response) {
-    // handle success
-    // console.log(response.data);
 
-    let data = response.data;
-    // get object keys from data
-    let keys = Object.keys(data);
-    // console.log(keys);
-    let products = [];
-    keys.forEach(key => {
-      products.push(data[key]);
+function getApi() {
+  axios.get('https://pubpup-tour-default-rtdb.asia-southeast1.firebasedatabase.app/products.json')
+    .then(function (response) {
+      // handle success
+      // console.log(response.data);
+
+      let data = response.data;
+      // get object keys from data
+      let keys = Object.keys(data);
+      // console.log(keys);
+      let products = [];
+      keys.forEach(key => {
+        products.push(data[key]);
+      });
+      console.log(products);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
     });
-    console.log(products);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
 }
 
 function displayProducts(products) {
-
   let data = getApi(products);
 
   let imageContainer = document.createElement('div');
@@ -33,8 +33,8 @@ function displayProducts(products) {
   let showImageDiv = document.createElement('div');
   showImageDiv.className = 'showImage';
   let image = document.createElement('img');
-  image.src = data.image.src;
-  image.alt = data.image.alt;
+  image.src = products.image.src;
+  image.alt = products.image.alt;
   showImageDiv.appendChild(image);
 
   let imageDetailsDiv = document.createElement('div');
@@ -51,7 +51,7 @@ function displayProducts(products) {
   ratingDiv.className = 'rating';
   let starRatingDiv = document.createElement('div');
   starRatingDiv.className = 'starRating';
-  starRatingDiv.textContent = data.rating,score;
+  starRatingDiv.textContent = data.rating, score;
   let reviewCountspan = document.createElement('span');
   reviewCountspan.className = 'reviewCount';
   reviewCountspan.textContent = data.rating.reviewCount;
@@ -66,8 +66,8 @@ function displayProducts(products) {
   let durationLi = document.createElement('li');
   durationLi.className = 'duration';
   durationLi.textContent = data.duration.day + 'Days' + data.duration.hours + 'hours' + data.duration.minutes + 'minutes';
- fetureUl.appendChild(behavioursLi);
- fetureUl.appendChild(durationLi);
+  fetureUl.appendChild(behavioursLi);
+  fetureUl.appendChild(durationLi);
 
   let priceDiv = document.createElement('div');
   let priceSpan = document.createElement('span');
@@ -90,18 +90,13 @@ function displayProducts(products) {
 
   // document.body.appendChild(imageContainer);
 
-  document.getElementsByClassName('image-container').appendChild(imageContainer);
+  document.querySelector('.image-container').appendChild(imageContainer); // Use querySelector to select the first element with the class name 'image-container'
   console.log('displayProducts');
   console.log(data);
   console.log('displayProducts');
   console.log(data);
 
-
-
-
-
-
-
-}
+};
 
 displayProducts();
+
