ba7013f9bdb5f0098475bf87c4a2e5f53cf01487
Ratchadapond <ratchadapornhomjan@gmail.com>
2024-03-27 16:31:00 +0700

    Table

--- script.js
+++ script.js
@@ -29,49 +29,8 @@ document.addEventListener("DOMContentLoaded", function() {
       updateSummary();
   }
 
-  
   // เพิ่มรายการรายรับ-รายจ่ายตัวอย่าง
   addTransaction('2024-03-27', 'เงินเดือน', 30000, 0);
   addTransaction('2024-03-28', 'ชำระค่าเช่า', 0, 7000);
 });
 
-// สมมติให้ income และ expense เป็นตัวแปรที่เก็บยอดรวมรายรับและรายจ่ายตามลำดับ
-const income = 50000;
-const expense = 20000;
-
-// เลือก canvas element ที่เราสร้างขึ้นมาให้แสดงผล
-const ctx = document.getElementById('myChart').getContext('2d');
-
-// สร้างข้อมูลสำหรับกราฟวงกลม
-const data = {
-  labels: ['รายรับ', 'รายจ่าย'],
-  datasets: [{
-    label: 'รายรับและรายจ่าย',
-    data: [income, expense],
-    backgroundColor: [
-      'rgba(75, 192, 192, 0.2)', // สีสำหรับรายรับ
-      'rgba(255, 99, 132, 0.2)' // สีสำหรับรายจ่าย
-    ],
-    borderColor: [
-      'rgba(75, 192, 192, 1)',
-      'rgba(255, 99, 132, 1)'
-    ],
-    borderWidth: 1
-  }]
-};
-
-// กำหนดค่าอื่น ๆ ของกราฟ
-const options = {
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  }
-};
-
-// สร้างกราฟวงกลม
-const myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: data,
-  options: options
-});
